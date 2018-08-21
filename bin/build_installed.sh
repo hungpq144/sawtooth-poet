caba7db711cf99d1b0fb15bdbe3b0a88c1fff9a0
Tung Duong Thanh <tungdt@gemvietnam.com>
2018-08-21 23:12:02 +0700

    Update build script

--- bin/build_installed.sh
+++ bin/build_installed.sh
@@ -1,65 +1,22 @@
 #!/usr/bin/env bash
 
-#START_TIME=$SECONDS
-#echo "Build sawtooth-poet-engine"
-#docker build -f ../engine/Dockerfile-installed -t socoboy/installed-sawtooth-poet-engine:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build sawtooth-poet-engine in $ELAPSED_TIME seconds"
-#
-#START_TIME=$SECONDS
-#echo "Build poet-validator-registry-tp"
-#docker build -f ../families/Dockerfile-installed -t socoboy/installed-sawtooth-poet-validator-registry-tp:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build poet-validator-registry-tp in $ELAPSED_TIME seconds"
+if [[ -z $1 ]]
+then
+    build="latest"
+else
+    build=$1
+fi
 
-#START_TIME=$SECONDS
-#echo "Build sawtooth-settings-tp"
-#docker build -f ../families/settings/Dockerfile-installed -t socoboy/installed-sawtooth-settings-tp:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build sawtooth-settings-tp in $ELAPSED_TIME seconds"
-#
-#START_TIME=$SECONDS
-#echo "Build sawtooth-block-info-tp"
-#docker build -f ../families/block_info/Dockerfile-installed -t socoboy/installed-sawtooth-block-info-tp:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build sawtooth-block-info-tp in $ELAPSED_TIME seconds"
-#
-#START_TIME=$SECONDS
-#echo "Build sawtooth-identity-tp"
-#docker build -f ../families/identity/Dockerfile-installed -t socoboy/installed-sawtooth-identity-tp:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build sawtooth-identity-tp in $ELAPSED_TIME seconds"
-#
-#START_TIME=$SECONDS
-#echo "Build sawtooth-intkey-tp-python"
-#docker build -f ../sdk/examples/intkey_python/Dockerfile-installed -t socoboy/installed-sawtooth-intkey-tp-python:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build sawtooth-intkey-tp-python in $ELAPSED_TIME seconds"
-#
-#START_TIME=$SECONDS
-#echo "Build sawtooth-xo-tp-python"
-#docker build -f ../sdk/examples/xo_python/Dockerfile-installed -t socoboy/installed-sawtooth-xo-tp-python:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build sawtooth-xo-tp-python in $ELAPSED_TIME seconds"
-#
-#START_TIME=$SECONDS
-#echo "Build sawtooth-smallbank-tp-go"
-#docker build -f ../families/smallbank/smallbank_go/Dockerfile-installed -t socoboy/installed-sawtooth-smallbank-tp-go:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build sawtooth-smallbank-tp-go in $ELAPSED_TIME seconds"
+echo "---------- BUILD IMAGES VERSION $build -------------"
 
-#START_TIME=$SECONDS
-#echo "Build sawtooth-smallbank-tp-rust"
-#docker build -f ../families/smallbank/smallbank_rust/Dockerfile-installed -t socoboy/installed-sawtooth-smallbank-tp-rust:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build sawtooth-smallbank-tp-rust in $ELAPSED_TIME seconds"
+START_TIME=$SECONDS
+echo "Build sawtooth-poet-engine"
+docker build -f ../engine/Dockerfile-installed -t socoboy/installed-sawtooth-poet-engine:$build ../
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed build sawtooth-poet-engine in $ELAPSED_TIME seconds"
 
-#START_TIME=$SECONDS
-#echo "Build sawtooth-devmode-rust"
-#docker build -f ../sdk/examples/devmode_rust/Dockerfile-installed -t socoboy/installed-sawtooth-devmode-rust:latest ../
-#ELAPSED_TIME=$(($SECONDS - $START_TIME))
-#echo "Completed build sawtooth-devmode-rust in $ELAPSED_TIME seconds"
-
-
-
-
+START_TIME=$SECONDS
+echo "Build poet-validator-registry-tp"
+docker build -f ../families/Dockerfile-installed -t socoboy/installed-sawtooth-poet-validator-registry-tp:$build ../
+ELAPSED_TIME=$(($SECONDS - $START_TIME))
+echo "Completed build poet-validator-registry-tp in $ELAPSED_TIME seconds"
